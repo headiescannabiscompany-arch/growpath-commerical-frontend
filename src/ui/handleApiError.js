7771aff0de7c7ae8c63fe880b863b4bcf4da1dc8
GrowPath Dev <dev@growpath.com>
2026-02-21 15:06:23 -0500

    chore: re-export non-api JS twins

--- src/ui/handleApiError.js
+++ src/ui/handleApiError.js
@@ -1,6 +1,1 @@
-function getMessage(err) {
-  const data = getData(err);
-  const msg =
-    (data && (data.message || data.error || data.title)) || (err && err.message) || "";
-  return String(msg || "");
-}
+export * from "./handleApiError.ts";
