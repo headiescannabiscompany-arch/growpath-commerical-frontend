fc995de22a4a1807346653f728e8ab6eb5914c67
GrowPath Dev <dev@growpath.com>
2026-07-31 00:24:39 -0400

    Fix Render startup entry

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
-import "expo-router/entry-classic.js";
+import "@expo/metro-runtime";
+import { App } from "expo-router/build/qualified-entry.js";
+import { renderRootComponent } from "expo-router/build/renderRootComponent.js";
+
+renderRootComponent(App);
